a9f452d6071273bb2c2db21ee9853adc0e093609
Maure Andrade <maure.andrade@academico.ifrn.edu.br>
2024-07-08 14:53:48 -0300

    muda o nome de Pergunta para Questao

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -2,7 +2,7 @@ from .common import Common
 from .alternativa import Alternativa
 from .participante import Participante
 from .partida import Partida
-from .pergunta import Pergunta
+from .questao import Questao
 from .questionario import Questionario
 from .resposta import Resposta
 from .usuario import Usuario
