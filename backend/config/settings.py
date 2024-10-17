85448f8580bc635aa8b9d46ccbeb01055666a69f
Maure Andrade <maure.andrade@academico.ifrn.edu.br>
2024-10-17 14:38:25 -0300

    adiciona channels

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,6 +20,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -30,6 +32,8 @@ INSTALLED_APPS = [
     'api',
     'corsheaders'
 ]
+
+ASGI_APPLICATION = 'config.asgi.application'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
