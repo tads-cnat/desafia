bd17f6009cb0aa4e62216afa8678e8728305acde
Livia-Rachell <liviarachelc@gmail.com>
2025-01-17 20:37:48 -0300

    fix: problem with dynamic css classes

--- frontend/src/features/ProfessorDashboard/Jogo/ExibirQuestao.tsx
+++ frontend/src/features/ProfessorDashboard/Jogo/ExibirQuestao.tsx
@@ -24,48 +24,51 @@ function ExibirQuestao({
 
     const { alternativas } = questao || {};
 
-    const alternativasArray = alternativas?.map((alternativa, index) => {
-        return { ...alternativa, color: alternativaColor[index] };
-    });
+    const getClassName = (
+        color: string,
+        correta: boolean
+    ): string => {
+        const baseClass =
+            "p-10 text-4xl font-semibold rounded-xl dark:text-neutral-50";
+        const colorClass =
+            state === GameState.TIMES_UP && !correta
+                ? `${color} opacity-20`
+                : color;
+        return `${baseClass} ${colorClass}`;
+    };
 
     return (
-        <>
-            <div className="flex flex-col justify-around min-h-screen items-center">
-                <button
-                    className="btn btn-primary self-end me-4"
-                    onClick={onNextQuestion}
-                >
-                    Próxima Questão
-                </button>
-                <div className="dark:bg-neutral-900/80 p-8 w-full">
-                    <h1 className="text-6xl text-center font-bold dark:text-neutral-50 text-neutral-900">
-                        {questao?.enunciado}
-                        {"   "}
-                        <Countdown counter={10} onZero={onZero} />
-                    </h1>
-                </div>
+        <div className="flex flex-col justify-around min-h-screen items-center">
+            <button
+                className="btn btn-primary self-end me-4"
+                onClick={onNextQuestion}
+            >
+                Próxima Questão
+            </button>
+            <div className="dark:bg-neutral-900/80 p-8 w-full">
+                <h1 className="text-6xl text-center font-bold dark:text-neutral-50 text-neutral-900">
+                    {questao?.enunciado}
+                    {"   "}
+                    <Countdown counter={10} onZero={onZero} />
+                </h1>
+            </div>
 
-                <div className="grid">
-                    <div className="grid grid-cols-2 gap-2 ">
-                        {alternativasArray?.map((alternativa, index) => (
-                            <div
-                                key={index}
-                                className={
-                                    alternativa.color +
-                                    " p-10 text-4xl font-semibold rounded-xl dark:text-neutral-50 " +
-                                    (state === GameState.TIMES_UP &&
-                                    !alternativa.correta
-                                        ? `${alternativa.color}/20`
-                                        : "")
-                                }
-                            >
-                                {alternativa.texto}
-                            </div>
-                        ))}
-                    </div>
+            <div className="grid">
+                <div className="grid grid-cols-2 gap-2">
+                    {alternativas?.map((alternativa, index) => (
+                        <div
+                            key={index}
+                            className={getClassName(
+                                alternativaColor[index],
+                                alternativa.correta
+                            )}
+                        >
+                            {alternativa.texto}
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
